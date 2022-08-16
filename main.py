97c88b47b8627d54b4413676d4220ae65c06fe0d
manukrizz322 <manukrizz322@gmail.com>
2022-08-16 17:28:07 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,91 @@
+
+from flask import Flask,request,jsonify 
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+import os
+
+app=Flask(__name__)
+
+"""
+@app.route('/',methods=['GET'])
+def home():
+    return jsonify(
+        
+        {'msg':'Welcome to Tutor Joes',
+        'name':'Tutor Joes'}
+        )
+"""
+basedir=os.path.abspath(os.path.dirname(__file__))
+print('Hello',basedir)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+ma=Marshmallow(app)
+
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100))
+    contact=db.Column(db.String(100),unique=True)
+
+    def __init__(self,name,contact):
+        self.name=name
+        self.contact=contact
+
+class UserScheme(ma.Schema):
+    class Meta:
+        fields=('id','name','contact')
+
+user_schema=UserScheme()
+users_schema=UserScheme(many=True)
+
+# Add New User
+@app.route('/user',methods=['POST'])
+def add_user():
+    name=request.json['name']
+    contact=request.json['contact']
+    new_user=User(name,contact)
+    db.session.add(new_user)
+    db.session.commit()
+    return user_schema.jsonify(new_user)
+
+#Show All User
+@app.route('/user',methods=['GET'])
+def getAllUser():
+    all_Users=User.query.all()
+    result=users_schema.dump(all_Users)
+    return jsonify(result)
+
+# Show User by ID
+@app.route('/user/<id>',methods=['GET'])
+def getUserByid(id):
+    user=User.query.get(id)
+    return user_schema.jsonify(user)
+
+# Update User By ID
+@app.route('/user/<id>',methods=['PUT'])
+def UpdateUser(id):
+    user=User.query.get(id)
+    name=request.json['name']
+    contact=request.json['contact']
+    user.name=name
+    user.contact=contact
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+#Delete User By ID
+@app.route('/user/<id>',methods=['DELETE'])
+def DeleteUserById(id):
+    user=User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+
+    
+    
+if __name__=='__main__':
+    app.run(debug=True)
